08df68fc6eed93b443bc8e28a25dbbbc909fb64b
이동현 <idleh4021@naver.com>
2024-11-10 16:49:36 +0900

    set up default code

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,30 @@ namespace ModalPopup
         {
             InitializeComponent();
         }
+
+        public int parentX, parentY;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form modalBackground = new Form();
+            using(Modal modal = new ModalPopup.Modal())
+            {
+                modalBackground.StartPosition = FormStartPosition.Manual;
+                modalBackground.FormBorderStyle = FormBorderStyle.None;
+                modalBackground.Opacity = .50d;
+                modalBackground.BackColor = Color.Black;
+                modalBackground.Size = this.Size;
+                modalBackground.Location = this.Location;
+                modalBackground.ShowInTaskbar = false;
+                modalBackground.Show();
+                modal.Owner = modalBackground;
+
+                parentX = this.Location.X;
+                parentY = this.Location.Y;
+
+                modal.ShowDialog();
+                modalBackground.Dispose();
+            }
+            
+        }
     }
 }
---
+++ Modal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModalPopup
+{
+    public partial class Modal : Form
+    {
+        int timerInterval;
+        public Modal()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.Opacity >= 1)
+            {
+                timer1.Stop();
+            }
+            else { Opacity += .03; }
+        }
+
+        private void Modal_Load(object sender, EventArgs e)
+        {
+            //this.Location = new Point()
+        }
+    }
+}
